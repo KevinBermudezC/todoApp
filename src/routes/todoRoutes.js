8c048cd38fc9ade72d057459d36c47593b5423e3
KevinBermudezC <kevinbermudezc@gmail.com>
2024-12-04 22:12:13 -0500

    :building_construction: Create routes and db

---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import db from "../db.js";
+
+const router = express.Router();
+//get all todos for logged-in user
+router.get('/',(req,res) => {
+
+})
+
+//create a todo for logged-in user
+router.post('/',(req,res) => {
+
+})
+
+//update a todo for logged-in user
+router.put('/:id',(req,res) => {
+
+})
+
+//delete a todo for logged-in user
+router.delete('/',(req,res) => {
+
+})
+
+export default router;
